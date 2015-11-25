ea8c97f0767b23a90ec9130dc897d078d210f5d7
Brian Eugene Smith <brianesmith11@gmail.com>
2015-11-25 00:37:02 -0600

    new route

--- app/views.py
+++ app/views.py
@@ -1,23 +1,31 @@
 from app import app
 from app.manipulate import Name
-# from app.forms import NameForm
+from app.forms import NameForm
 from flask import render_template,redirect,url_for,request
-# from flask.ext.bootstrap import Bootstrap
-from twilio import twiml
+from flask.ext.bootstrap import Bootstrap
+from twilio import twilio
 
-# bootstrap = Bootstrap(app)
+bootstrap = Bootstrap(app)
 
 
-@app.route('/',methods=['POST'])
+@app.route('/',methods=['GET','POST'])
 def index():
+    form = NameForm()
+    if form.is_submitted():
+        return redirect(url_for('scramble',name=form.name.data))
+    return render_template('index.html',form=form)
+
+@app.route('/scramble/<name>')
+def scramble(name):
+    name = Name(name)
+    return render_template('scramble.html',name=name)
+
+@app.route('/sms',methods=['POST'])
+def sms():
     response = twiml.Response()
     body = request.form['Body']
 
-    response.message("You sent me: {0}".format(body))
+    name = Name(body)
 
+    response.message("pig latin:{0}\n,backwards:{1}".format(name.piglatin(),name.backwards()))
     return str(response)
-
-# @app.route('/scramble/<name>')
-# def scramble(name):
-#     name = Name(name)
-#     return render_template('scramble.html',name=name)
