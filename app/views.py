11107756cb24d142ed6fb24d931afeb2476e2604
Brian Eugene Smith <brianesmith11@gmail.com>
2015-11-25 00:12:03 -0600

    twilio added

--- app/views.py
+++ app/views.py
@@ -1,20 +1,23 @@
 from app import app
 from app.manipulate import Name
-from app.forms import NameForm
-from flask import render_template,redirect,url_for
-from flask.ext.bootstrap import Bootstrap
+# from app.forms import NameForm
+from flask import render_template,redirect,url_for,request
+# from flask.ext.bootstrap import Bootstrap
+from twilio import twiml
 
-bootstrap = Bootstrap(app)
+# bootstrap = Bootstrap(app)
 
 
-@app.route('/',methods=['GET','POST'])
+@app.route('/',methods=['POST'])
 def index():
-    form = NameForm()
-    if form.is_submitted():
-        return redirect(url_for('scramble',name=form.name.data))
-    return render_template('index.html',form=form)
+    response = twiml.Response()
+    body = request.form['Body']
 
-@app.route('/scramble/<name>')
-def scramble(name):
-    name = Name(name)
-    return render_template('scramble.html',name=name)
+    response.message("You sent me: {0}".format(body))
+
+    return str(response)
+
+# @app.route('/scramble/<name>')
+# def scramble(name):
+#     name = Name(name)
+#     return render_template('scramble.html',name=name)
